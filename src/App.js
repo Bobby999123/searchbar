5a26748dcac040ffe4a8c9fb55e4c8918dae5ca1
pankajrajput0312 <pankajrajput020010@gmail.com>
2021-08-04 14:55:05 +0530

    initial comment

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Search from './Search';
+
+function App() {
+
+
+
+  return (
+    <div className="app">
+      <Search />
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Search.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Search.css';
+import SearchBar from './SearchBar';
+
+function Search() {
+
+
+    return (
+        <div>
+            <SearchBar />
+
+
+
+        </div>
+    )
+}
+
+export default Search;
